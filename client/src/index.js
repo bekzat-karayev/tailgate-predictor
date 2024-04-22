a535a5de6348051a459a97b506c2fa71e3c290c8
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-04-22 19:02:03 +0500

    Add login page

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import store from './store/store';
 import App from './components/App';
+import Login from './components/Login'
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 const Page = () => (
   <Provider store={store}>
-  <App />
+  <Login />
  </Provider>
 );
 
